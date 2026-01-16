63eb447d18cb55e49123f4417239d57e5e0eea28
Andrej <45800426+anfocic@users.noreply.github.com>
2026-01-16 18:07:02 +0000

    A bit refactoring and styling improvement.

--- src/config/navLinks.ts
+++ src/config/navLinks.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
-    { path: "/services", label: "What we do" },
-    { path: "/about", label: "About" },
-    { path: "/contact" , label: "Contact" },
-    { path: "/blog", label: "Blog" },
+    { path: "/services", label: "What we do" }
+    // { path: "/about", label: "About" },
+    // { path: "/contact" , label: "Contact" },
+    // { path: "/blog", label: "Blog" },
 ];
