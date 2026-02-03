21b0fb0d706e0e7b802e8b0c60e9707bd9d53c91
Andrej <45800426+anfocic@users.noreply.github.com>
2026-02-03 18:38:07 +0000

    Removed lucide icons and unused code.

--- public/scripts/easter-egg.js
+++
@@ -1,8 +0,0 @@
-(() => {
-    const REVEAL_AT_MS = 3800;
-
-    const egg = document.querySelector(".hero-easteregg");
-    if (egg) {
-        window.setTimeout(() => egg.classList.add("is-visible"), REVEAL_AT_MS);
-    }
-})()
